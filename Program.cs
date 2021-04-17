eb9b63739b15596deefd3a85db7ade42f306476c
luxlinna <luxlinna@gmail.com>
2021-04-17 23:19:05 +0200

    add all files

--- Program.cs
+++ Program.cs
@@ -9,42 +9,49 @@ namespace Avrundaren
         static void Main(string[] args)
         {
             double results;
-
             Console.ForegroundColor = ConsoleColor.Yellow;
-            Console.Write("Skriva in decimaltal: ");
-            double num1 = Convert.ToDouble(Console.ReadLine());
+            try 
+            {
+                Console.Write("Skriva in decimaltal: ");
+                double num1 = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("\nSkriva in annat decimaltal: ");
-            double num2 = Convert.ToDouble(Console.ReadLine());
+                Console.Write("\nSkriva in annat decimaltal: ");
+                double num2 = Convert.ToDouble(Console.ReadLine());
 
-            Console.Write("\nSkriva in operatör tecknet: ");
-            string op = Console.ReadLine();
+                Console.Write("\nSkriva in operatör tecknet: ");
+                string op = Console.ReadLine();
 
-            if (op == "+")
+                if (op == "+")
+                {
+                    results = Math.Round(num1, 5) + Math.Round(num2, 5);
+                    Console.WriteLine("\nResultatet är : " + Math.Round(num1, 5) + " + " + Math.Round(num2, 5) + " = " + Math.Round(results, 3));
+                    Console.ReadKey();
+                } else if (op == "-")
+                {
+                    results = Math.Round(num1, 5) - Math.Round(num2, 5);
+                    Console.WriteLine("\nResultatet är : " + Math.Round(num1, 5) + " - " + Math.Round(num2, 5) + " = " + Math.Round(results, 3));
+                    Console.ReadKey();
+                } else if (op == "*")
+                {
+                    results = Math.Round(num1, 5) * Math.Round(num2, 5);
+                    Console.WriteLine("\nResultatet är : " + Math.Round(num1, 5) + " * " + Math.Round(num2, 5) + " = " + Math.Round(results, 3));
+                    Console.ReadKey();
+                } else if (op == "/")
+                {
+                    results = Math.Round(num1, 5) / Math.Round(num2, 5);
+                    Console.WriteLine("\nResultatet är : " + Math.Round(num1, 5) + " / " + Math.Round(num2, 5) + " = " + Math.Round(results, 3));
+                    Console.ReadKey();
+                } else
+                {
+                    Console.WriteLine("\nOperatör tecknet är fel !!!");
+                    Console.ReadKey();
+                } 
+            }
+            catch(Exception e)
             {
-                results = Math.Round(num1, 5) + Math.Round(num2, 5);
-                Console.WriteLine("\nResultatet är : " + Math.Round(num1, 5) + " + " + Math.Round(num2, 5) + " = " + Math.Round(results, 3));
+                Console.WriteLine(e.Message);
                 Console.ReadKey();
-            } else if (op == "-")
-            {
-                results = Math.Round(num1, 5) - Math.Round(num2, 5);
-                Console.WriteLine("\nResultatet är : " + Math.Round(num1, 5) + " - " + Math.Round(num2, 5) + " = " + Math.Round(results, 3));
-                Console.ReadKey();
-            } else if (op == "*")
-            {
-                results = Math.Round(num1, 5) * Math.Round(num2, 5);
-                Console.WriteLine("\nResultatet är : " + Math.Round(num1, 5) + " * " + Math.Round(num2, 5) + " = " + Math.Round(results, 3));
-                Console.ReadKey();
-            } else if (op == "/")
-            {
-                results = Math.Round(num1, 5) / Math.Round(num2, 5);
-                Console.WriteLine("\nResultatet är : " + Math.Round(num1, 5) + " / " + Math.Round(num2, 5) + " = " + Math.Round(results, 3));
-                Console.ReadKey();
-            } else
-            {
-                Console.WriteLine("\nOperatör tecknet är fel !!!");
-                Console.ReadKey();
-            }    
+            }
         }
     }
 }
